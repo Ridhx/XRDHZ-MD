760f62a5fbf5f41e5119e0bf6cf4eb0b702d1b2c
Ridevx <189964031+Ridhx@users.noreply.github.com>
2025-10-10 12:13:22 +0800

    Update print.js

--- function/print.js
+++ function/print.js
@@ -6,24 +6,28 @@ import { unwatchFile, watchFile, readFileSync } from "fs";
 
 export default async function (m, conn = { user: {} }) {
     if (m.fromMe) return;
-    const senderName = await conn.getName(m.sender);
-    const groupName = await conn.getName(m.chat);
+    const _name = await conn.getName(m.sender);
+    const _chat = await conn.getName(m.chat);
     const sender = await parsePhoneNumber("+" + m.sender.replace("@s.whatsapp.net", ""))?.number
         ?.international;
 
     let user = global.db.data?.users[m.sender];
-    let chat = global.db.data?.chats[m.chat];
 
-    let render = `${chalk.white(" » SENDER:")} ${chalk.yellow("%s")}\n`;
-    render += `${chalk.white(" » DATE:")} ${chalk.yellow("%s")}`;
+    let render = `${chalk.white(" » SENDER:")} ${chalk.white("%s")}\n`;
+    render += `${chalk.white(" » NAME :")} ${chalk.blue("%s")}\n`;
+    render += `${chalk.white(" » DATE:")} ${chalk.gray("%s")}\n`;
+    render += `${chalk.white(" » SEND TO:")} ${chalk.green("%s")}`;
+
     console.log(chalk.gray("-".repeat(50)));
     console.log(
         render,
+        _name,
         sender,
         (m.messageTimestamp
             ? new Date(1000 * (m.messageTimestamp.low || m.messageTimestamp))
             : new Date()
-        ).toLocaleString("id", { timeZone: "Asia/Jakarta" })
+        ).toLocaleString("id", { timeZone: "Asia/Jakarta" }),
+        m.chat + (_chat ? "~" + _chat : "")
     );
     console.log(chalk.gray("-".repeat(50)));
 }
@@ -32,4 +36,5 @@ let file = fileURLToPath(import.meta.url);
 watchFile(file, async () => {
     unwatchFile(file);
     console.log(`${chalk.white.bold(" [SISTEM]")} ${chalk.green.bold(`FILE DIUPDATE "print.js"`)}`);
+    import(`${file}?update=${Date.now()}`);
 });
