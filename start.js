651c369f4b6d1e36e3d5bed1e4710c5462745773
Ridhx <189964031+Ridhx@users.noreply.github.com>
2025-11-06 14:51:44 +0800

    Update: start.js

--- start.js
+++ start.js
@@ -12,20 +12,37 @@ import readline from "readline";
 import { headerLog } from "./function/console.js";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const sleep = ms => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
 
+let isRunning = false;
 const startBot = async () => {
+    if (isRunning) return;
+    isRunning = true;
     const args = [path.join(__dirname, "main.js"), ...process.argv.slice(2)];
     const p = spawn(process.argv[0], args, {
         stdio: ["inherit", "inherit", "inherit", "ipc"]
     });
 
     p.on("exit", code => {
-        console.log(chalk.red(`-- `));
-        if (code === 1 || code === 0) startBot();
+        console.log(`${chalk.white.bold(" [SISTEM]")} ${chalk.yellow.bold(`BERHENTI DENGAN KODE: ${code}`)}`);
+        isRunning = false;
+        if (code === 1 || code === 0) {
+            sleep(600);
+            startBot();
+        }
     });
+
+    p.on("error", err => {
+        console.log(`${chalk.white.bold(" [SISTEM]")} ${chalk.red.bold(`ERROR ❌`)}`);
+        console.error(err);
+        p.kill();
+        isRunning = false;
+        sleep(600);
+        startBot();
+    });
+    await headerLog();
 };
 
-// BELUM SELESAI :3
-
-await headerLog();
 await startBot();
