2a773065c4e3ec4158086226401dfa2dd020a52a
X <159207241+XM4ZE@users.noreply.github.com>
2025-11-12 15:22:59 +0700

    Update and rename _test.js to owner-sf.js

--- plugins/_test.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, text }) => {
-    return m.reply("test")
-};
-
-handler.help = ["test"];
-handler.command = /^(test)$/i;
-handler.premium = true;
-
-export default handler;
